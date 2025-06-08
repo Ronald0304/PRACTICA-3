03defb15a83b580af06ac48c9845ed2cc495a743
Ronald Paul Gutierrez Mamani <160867694+Ronald0304@users.noreply.github.com>
2025-06-08 19:59:32 -0400

    Update ej1.py

--- GENERECIDAD /Ejercicio1/codigos/ej1.py
+++ GENERECIDAD /Ejercicio1/codigos/ej1.py
@@ -8,7 +8,7 @@ T = TypeVar('T')
 
 class Caja(Generic[T]):
     def __init__(self):
-        self.contenido: T = None  # Inicializa el contenido
+        self.contenido: T = None 
 
     def guardar(self, objeto: T):
         self.contenido = objeto
@@ -16,15 +16,11 @@ class Caja(Generic[T]):
     def obtener(self) -> T:
         return self.contenido
 
-def main():
-    caja_texto = Caja[str]()
-    caja_texto.guardar("Hola Mundo")
+caja_texto = Caja[str]()
+caja_texto.guardar("Hola Mundo")
 
-    caja_numero = Caja[int]()
-    caja_numero.guardar(12345)
+caja_numero = Caja[int]()
+caja_numero.guardar(12345)
 
-    print("Contenido de caja_texto:", caja_texto.obtener())
-    print("Contenido de caja_numero:", caja_numero.obtener())
-
-if __name__ == "__main__":
-    main()
+print("Contenido de caja de texto:", caja_texto.obtener())
+print("Contenido de caja de numero:", caja_numero.obtener())
